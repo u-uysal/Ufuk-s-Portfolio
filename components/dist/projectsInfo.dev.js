54a79dc7eb6bafad1a065c62f679888da4f5dd58
ufuk <ufuk.uysl5@gmail.com>
2020-08-25 11:59:19 +0200

    Add quote generator project

---
+++ components/dist/projectsInfo.dev.js
@@ -0,0 +1,65 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var projectsInfo = [{
+  id: 1,
+  src: "/dance.png",
+  alt: "A couple are dancing",
+  projectName: "Dance With Me",
+  sourceCodeLink: "https://github.com/Mert1980/DanceWithMe",
+  liveModeLink: "https://hyf-class6-dancewithme.herokuapp.com/",
+  intro: "Dance-With-Me application matches the users in accordance with their preferences (location, gender, age, weight and height) and show the events in which they can enjoy together."
+}, {
+  id: 2,
+  src: "/cards.png",
+  alt: "Github interface",
+  projectName: "Github Cards App",
+  sourceCodeLink: "https://github.com/u-uysal/github-card-app",
+  liveModeLink: "https://u-uysal.github.io/github-card-app/",
+  intro: "Github cards app finds GitHub user according to user name by using Github API"
+}, {
+  id: 3,
+  src: "/form.png",
+  alt: "Sign up form",
+  projectName: "React Form Validation",
+  sourceCodeLink: "https://github.com/u-uysal/Form-Validation",
+  liveModeLink: "https://u-uysal.github.io/Form-Validation/index.html",
+  intro: "React Form Validation is an app that user inputs own information for a random application"
+}, {
+  id: 4,
+  src: "/todo.png",
+  alt: "To do list",
+  projectName: "React Todo List",
+  sourceCodeLink: "https://github.com/u-uysal/TodoList",
+  liveModeLink: "https://u-uysal.github.io/TodoList/",
+  intro: "By using this app, you can add work to do , update this works and delete after completion"
+}, {
+  id: 5,
+  src: "/four-card.png",
+  alt: "Four Card Application",
+  projectName: "Four Cards Challenge",
+  sourceCodeLink: "https://github.com/u-uysal/four-card",
+  liveModeLink: "https://u-uysal.github.io/four-card/",
+  intro: "Four Card Challenge is built by using HTML5-CSS3 "
+}, {
+  id: 6,
+  src: "/covid.png",
+  alt: "Covid-19",
+  projectName: "Covid Tracker App",
+  sourceCodeLink: "https://github.com/u-uysal/covid-tracker-app",
+  liveModeLink: "https://u-uysal.github.io/covid-tracker-app/",
+  intro: "The main purpose of this project is to provide accurate information to those who want to get information about Covid -19."
+}, {
+  id: 7,
+  src: "/quote.png",
+  alt: "Quotes",
+  projectName: "Quote Generator App",
+  sourceCodeLink: "https://github.com/u-uysal/Queto-Generator",
+  liveModeLink: "https://u-uysal.github.io/Queto-Generator/index.html",
+  intro: "By using Quteos API, You can see nice quotes from famous persons and share your twitter"
+}];
+var _default = projectsInfo;
+exports["default"] = _default;
